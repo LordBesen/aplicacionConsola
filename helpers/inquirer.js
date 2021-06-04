7badabc15484d4083fb6786348d9ff68e48a91f5
Federico Besenzoni <besen@hotmail.com>
2021-06-04 18:32:55 -0300

    clase 50

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -6,8 +6,50 @@ const preguntas = [
         type:'list',
         name:'opcion',
         message: 'QUe desea hacer?',
-        choices: ['1. Crear lista','opt2','opt3']
-    }
+        choices: [
+            {
+
+                value:'1',
+                name: '1. Crear tarea'
+            },
+            {
+
+                value:'2',
+                name: '2. Listar tareas'
+            },
+            {
+
+                value:'3',
+                name: '3. Listar tareas completadas'
+            },
+            {
+
+                value:'4',
+                name: '4. Listar tareas pendientes'
+            },
+            {
+
+                value:'5',
+                name: '5. Completar tareas'
+
+            },
+            {
+
+                value:'6',
+                name: '6. Borrar tareas'
+
+            },
+            {
+
+                value:'0',
+                name: '0. Salir'
+
+            },
+
+        ]
+
+
+}]
 
 
 
@@ -17,11 +59,6 @@ const preguntas = [
 
 
 
-
-
-
-
-];
 
 
 const inquirerMenu = async () => {
@@ -31,9 +68,9 @@ const inquirerMenu = async () => {
     console.log('=== Seleccione una opcion ==')
     console.log('============================')
      
-   const opt = await inquirer.prompt(preguntas);
+   const {opcion} = await inquirer.prompt(preguntas);
 
-    return opt;
+    return opcion;
 
 
    
